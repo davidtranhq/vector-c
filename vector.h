f3b38e9aa33b6ec28bf343234ea4caae1cf33c10
hydratedcabbage <hydratedcabbage@gmail.com>
2019-05-29 20:01:11 -0400

    Update vector.h

--- vector.h
+++ vector.h
@@ -13,3 +13,40 @@ typedef enum vector_errors
 	VEC_BAD_ALLOC,
 	VEC_OUT_OF_RANGE,
 } Vector_error;
+
+/*
+Usage:
+
+#include "vector.h"
+
+#define TYPE int
+#include "vector.t"
+#undef TYPE
+
+#define TYPE char
+#include "vector.t"
+#undef TYPE
+
+int main()
+{
+	Vector_int *vi;
+	Vector_char *vc;
+	Vector_error e;
+	e = vec_create_int(&vi);
+	if (e != VEC_SUCCESS)
+	{
+		...
+	}
+	e = vec_create_char(&vc);
+	if (e != VEC_SUCCESS)
+	{
+		...
+	}
+	
+	...
+	
+	vec_free_int(&vi);
+	vec_free_char(&vc);
+	return 0;
+}
+*/
